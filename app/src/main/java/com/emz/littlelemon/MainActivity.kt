5c57e4566ccd377204a40b0e0bc58290d4d74d40
Eman Gaber <eg1712@gmail.com>
2024-10-09 23:40:44 +0300

    Data

--- app/src/main/java/com/emz/littlelemon/MainActivity.kt
+++ app/src/main/java/com/emz/littlelemon/MainActivity.kt
@@ -1,14 +1,88 @@
 package com.emz.littlelemon
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsChannel
+import io.ktor.client.statement.bodyAsText
+import io.ktor.client.statement.request
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromJsonElement
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MyNavigation()
+    val database by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            MenuDatabase::class.java,
+            "menu.db"
+        ).build()
+    }
+    private val client = HttpClient(Android){
+        install(ContentNegotiation){
+            json(contentType = ContentType("Text", "Plain"))
         }
     }
+    private val menuItemsLiveData = MutableLiveData<List<MenuItemNetwork>>(emptyList())
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
+            val menuItemsC = getMenu()
+            runOnUiThread {
+                menuItemsLiveData.value = menuItemsC
+            }
+        }
+        setContent {
+            MyNavigation()
+//            val menuItems by database.menuDao().getAllMenuItems()
+//                .observeAsState(emptyList())
+//            ItemsList(menuItems)
+        }
+    }
+    private suspend fun getMenu(): List<MenuItemNetwork> {
+        val baseUrl =
+            "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        val response: MenuNetwork =
+            client.get(baseUrl)
+                .body()
+
+        return response.menuItems
+    }
+
 }
+
+
